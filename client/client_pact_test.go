9340f7f4669c4fe4ff367807b399350e242aaf71
Matt Fellows <matt.fellows@onegeek.com.au>
2021-06-19 22:53:07 +1000

    wip: test removing dot import

--- client/client_pact_test.go
+++ client/client_pact_test.go
@@ -1,16 +1,16 @@
-// +build consumer
-
 package client
 
 import (
 	"testing"
 
 	"fmt"
+	"net/url"
+
 	"github.com/pact-foundation/pact-go/v2/consumer"
-	. "github.com/pact-foundation/pact-go/v2/sugar"
+	"github.com/pact-foundation/pact-go/v2/log"
+	"github.com/pact-foundation/pact-go/v2/matchers"
 	"github.com/pactflow/terraform/broker"
 	"github.com/stretchr/testify/assert"
-	"net/url"
 )
 
 func TestClientPact(t *testing.T) {
@@ -18,7 +18,7 @@ func TestClientPact(t *testing.T) {
 }
 
 func TestTerraformClientPact(t *testing.T) {
-	SetLogLevel("TRACE")
+	log.SetLogLevel("TRACE")
 
 	mockProvider, err := consumer.NewV2Pact(consumer.MockHTTPProviderConfig{
 		Consumer: "terraform-client",
@@ -33,16 +33,16 @@ func TestTerraformClientPact(t *testing.T) {
 		mockProvider.
 			AddInteraction().
 			UponReceiving("a request to create an application").
-			WithRequest("POST", S("/pacticipants")).
-			WithHeader("Content-Type", S("application/json")).
-			WithHeader("Authorization", Like("Bearer 1234")).
+			WithRequest("POST", matchers.S("/pacticipants")).
+			WithHeader("Content-Type", matchers.S("application/json")).
+			WithHeader("Authorization", matchers.Like("Bearer 1234")).
 			WithBodyMatch(&broker.Pacticipant{}).
 			WillRespondWith(200).
-			WithHeader("Content-Type", S("application/hal+json")).
+			WithHeader("Content-Type", matchers.S("application/hal+json")).
 			WithBodyMatch(&broker.Pacticipant{})
 
 			// Execute pact test
-		err = mockProvider.ExecuteTest(func(config MockServerConfig) error {
+		err = mockProvider.ExecuteTest(func(config consumer.MockServerConfig) error {
 			client := clientForPact(config)
 
 			p, e := client.CreatePacticipant(broker.Pacticipant{
@@ -62,7 +62,7 @@ func TestTerraformClientPact(t *testing.T) {
 // 	BaseURL:
 // }
 
-func clientForPact(config MockServerConfig) *Client {
+func clientForPact(config consumer.MockServerConfig) *Client {
 	baseURL, err := url.Parse(fmt.Sprintf("http://%s:%d", config.Host, config.Port))
 	fmt.Println(baseURL)
 	if err != nil {
